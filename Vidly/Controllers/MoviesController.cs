15bda90ded541626641bc99a81021ac445b75826
Rambod Ghashghai <gh.rambod@gmail.com>
2022-11-30 14:37:07 +0330

    Basics Test

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly/Controllers/SupportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            var support = new Support() {Name = "Rambod" ,Description="Nadarim hichi toosh" };
+            return View(support);
+        }
+    }
+}
